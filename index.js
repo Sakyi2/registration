2d007d01f1f146be589644d3f0cf3605c2b7d7de
Sakyi2 <sakyiisaiah4160@gmail.com>
2024-03-18 08:41:20 +0100

    run nodes

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ const dotenv = require('dotenv')
 
 const app = express();
 dotenv.config();
+
+
+const port = process.env.PORT || 3000
+
+app._get('/',(req,res) => {
+    res.sendfile(__dirname ="/pages/index.html")
+})
+
+
+app.post("/register", (req,res) => {
+    try{
+        const {name,email,password} =  req.body;
+    }
+    catch{
+
+    }
+})
+
+app.listen(port,()=> {
+    console.log(`server is ruuning on port ${port}`)
+})
